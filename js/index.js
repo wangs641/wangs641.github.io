a4f58c0ffae0b9c25a070900cfc534ed321b7059
Shuyang Wang <wangs641@newschool.edu>
2018-08-12 15:40:13 -0400

    0812js

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+        
+
+
+        $(window).scroll(function() {
+          $(".pageOne").fadeIn(1000);
+      
+        });
+        
+
+
+        });
---
+++ js/p1.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+        
+
+
+        $(window).scroll(function() {
+          
+          $(".photo2").fadeIn(6000);
+          $(".photo3").fadeIn(12000);
+          $(".photo4").fadeIn(16000);
+          $(".photo5").fadeIn(20000);
+          $(".photo6").fadeIn(24000);
+          $(".photo7").fadeIn(28000);
+          $(".photo8").fadeIn(32000);
+          $(".photo9").fadeIn(36000);
+          $(".photo10").fadeIn(40000);
+          $(".photo11").fadeIn(44000);
+          $(".photo12").fadeIn(50000);
+      
+        });
+        
+
+
+        });
